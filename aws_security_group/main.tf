dc541bd3787a1f7fffaa06c552b2738c2ba84111
Nikita Poplavskiy <poplavskiy96@gmail.com>
2021-10-26 14:26:52 +0300

    File To CREATE Security Group

---
+++ aws_security_group/main.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "webserver" {
+  name = "WebServer Security Group"
+  vpc_id = aws_vpc.prod-vpc.id
+
+
+  ingress   {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+
+  ingress  {
+    cidr_blocks = aws_network.vpc_cidr
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+  }
+
+  egress  {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+  }
+
+
+  tags = {
+    "Name" = "web-server-sg"
+    "Owner" = "Vatanabe"
+  }
+
+}
